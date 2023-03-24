b083b3dd92348f6e5ccf2d3f5e5b9b69083f201d
Miguel Santiago <mayestix12@gmail.com>
2023-03-24 00:33:10 -0700

    First commit

---
+++ src/main/java/com/paramodevs/paramoapi/ParamoApiApplication.java
@@ -0,0 +1,13 @@
+package com.paramodevs.paramoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParamoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParamoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paramodevs/paramoapi/ParamoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paramodevs.paramoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParamoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
