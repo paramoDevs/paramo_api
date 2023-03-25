b15dffd77dbfad73c6f66197acdbada8adbd37d9
Luis Chitala <53163045+luischitala@users.noreply.github.com>
2023-03-24 23:10:30 -0600

    Corrected env

--- src/main/java/com/paramodevs/paramoapi/ParamoApiApplication.java
+++ src/main/java/com/paramodevs/paramoapi/ParamoApiApplication.java
@@ -2,12 +2,20 @@ package com.paramodevs.paramoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ParamoApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParamoApiApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello(){
+		return "Hello World";
 	}
 
 }
