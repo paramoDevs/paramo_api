e9f6f401e972a774904fbfc0182c049f43beb940
Miguel Santiago <mayestix12@gmail.com>
2023-03-28 21:09:16 -0700

    Created User class, Controller and Repository. Missing CRUD methods.

---
+++ src/main/java/com/paramodevs/paramoapi/Controller.java
@@ -0,0 +1,5 @@
+package com.paramodevs.paramoapi;
+
+public class Controller {
+    //create, read, update, delete
+}
---
+++ src/main/java/com/paramodevs/paramoapi/Repository.java
@@ -0,0 +1,5 @@
+package com.paramodevs.paramoapi;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Repository extends JpaRepository<User, Integer> {
+}
